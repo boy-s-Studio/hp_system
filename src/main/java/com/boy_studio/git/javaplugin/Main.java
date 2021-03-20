5971ed06d2bcc98b00c08328fbdd30f377af94f5
son123j <boy0710boy@naver.com>
2021-03-20 14:22:06 +0900

    Main

--- src/main/java/com/boy_studio/git/javaplugin/Main.java
+++ src/main/java/com/boy_studio/git/javaplugin/Main.java
@@ -1,17 +1,16 @@
-package hp_system.hp_system;
+package com.boy_studio.git.javaplugin;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
-public final class Hp_system extends JavaPlugin {
+public final class Main extends JavaPlugin {
     
     @Override
     public void onEnable() {
-        // Plugin startup logic
-        
+    
     }
     
     @Override
     public void onDisable() {
-        // Plugin shutdown logic
+    
     }
 }
