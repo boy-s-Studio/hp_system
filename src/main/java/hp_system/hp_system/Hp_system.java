84a3d6ffc32889973f0dfecffef1c11d464c7bc1
son123j <boy0710boy@naver.com>
2021-03-20 14:13:12 +0900

    Initial commit

---
+++ src/main/java/hp_system/hp_system/Hp_system.java
@@ -0,0 +1,17 @@
+package hp_system.hp_system;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Hp_system extends JavaPlugin {
+    
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+        
+    }
+    
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
